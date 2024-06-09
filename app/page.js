024cd5022af5c6e2ff9f2e5bdda3a535e0083a76
unknown <atulkumar99707.ak13@gmail.com>
2024-06-09 23:25:08 +0530

    add main page and community page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,31 @@
+import Link from "next/link"
+import styles from "./page.module.css"
+import ImageSlideShow from "@/components/images/image-slideshow";
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-    </main>
+   <>
+   <header className={styles.header}>
+    <div className={styles.slideshow}>
+      <ImageSlideShow />
+    </div>
+    <div >
+      <div className={styles.hero}>
+        <h1>NextLevel Food for NextLevel Foodies</h1>
+        <p>Taste & share food from all over the world.</p>
+        
+      </div>
+      <div className={styles.cta}>
+        <Link href ="/community"> Join the Community</Link>
+        <Link href ="/meals">Explore Meals</Link>
+
+      </div>
+    </div>
+
+
+   </header>
+   <main>
+    
+   </main>
+   </>
   );
 }
